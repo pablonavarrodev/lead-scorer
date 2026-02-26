11b7b62a8f03ef42f3eddb429607f521f5c9525d
Pablo <pablonavarreitor@gmail.com>
2026-02-26 16:53:38 +0100

    chore: soporte para variables de entorno

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,16 @@
 #Rutas a los ficheros
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
-#Saco la ruta de este archivo, la paso a absoluta, y subo 2 escalones a la ruta padre. file es main.py
+#carga las variables del .env en el sistema para poder usarlas
+load_dotenv()
+
+#Saco la ruta de este archivo, la paso a absoluta, y subo 3 escalones a la ruta padre.
 BASE_DIR = Path(__file__).resolve().parents[2] 
-DATA_CSV = BASE_DIR / "data" / "leads.csv"
-OUTPUT_JSON = BASE_DIR / "output" / "leads_scored.json"
-OUTPUT_DIR = BASE_DIR / "output"
+
+#con getenv mandamos dos argumentos ya que por ejemplo el ci de github no tiene acceso al .env entonces asi sabe que ruta usar si no existe .env
+DATA_CSV = BASE_DIR / os.getenv("DATA_CSV_PATH", "data/leads.csv") 
+
+OUTPUT_DIR = BASE_DIR / os.getenv("OUTPUT_DIR", "output")
+OUTPUT_JSON = OUTPUT_DIR / os.getenv("OUTPUT_FILE", "leads_scored.json")
